24b70a979359f5889d864cf77fe0506048344b95
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-24 16:06:21 +0200

    Add files via upload

---
+++ Unique Usernames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count = int.Parse(Console.ReadLine());
+            HashSet<string> usernames = new HashSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                usernames.Add(Console.ReadLine());
+            }
+
+            foreach (string username in usernames)
+            {
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
