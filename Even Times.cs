572d48b2023bdc55a18516bf48279e5d7e842b8e
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-25 10:45:29 +0200

    Add files via upload

---
+++ Even Times.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count = int.Parse(Console.ReadLine());
+            Dictionary<int, int> numberOfAppearances = new Dictionary<int, int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                int inputNumber = int.Parse(Console.ReadLine());
+                if (!numberOfAppearances.ContainsKey(inputNumber))
+                {
+                    numberOfAppearances.Add(inputNumber, 0);
+                }
+                numberOfAppearances[inputNumber]++;
+
+            }
+            Console.WriteLine(string.Join(' ', numberOfAppearances
+                .Where(v => v.Value % 2 == 0)
+                .Select(n => n.Key)
+                ));
+            //.Values.Where(v => v % 2 == 0))
+        }
+    }
+}
