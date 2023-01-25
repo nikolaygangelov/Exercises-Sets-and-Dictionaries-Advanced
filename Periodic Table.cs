362536cd78ff3d28ae495266e120e285334b938d
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-25 09:37:42 +0200

    Add files via upload

---
+++ Periodic Table.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count = int.Parse(Console.ReadLine());
+            SortedSet<string> uniqueChemicals = new SortedSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] compounds = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < compounds.Length; j++)
+                {
+                    uniqueChemicals.Add(compounds[j]);
+                }
+                
+            }
+
+            Console.WriteLine(string.Join(' ', uniqueChemicals));
+
+        }
+    }
+}
