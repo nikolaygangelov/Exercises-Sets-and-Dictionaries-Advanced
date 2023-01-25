b8d0e6cce0862ab16e48e9d3f74d91fd1ef08304
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-25 11:15:40 +0200

    Add files via upload

---
+++ Count Symbols.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            SortedDictionary<char, int> countOfOccurrences = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentLetter = text[i];
+                if (!countOfOccurrences.ContainsKey(currentLetter))
+                {
+                    countOfOccurrences.Add(currentLetter, 0);
+                }
+                countOfOccurrences[currentLetter]++;
+            }
+
+            foreach (var (ch,count) in countOfOccurrences)
+            {
+                Console.WriteLine($"{ch}: {count} time/s");
+            }
+        }
+    }
+}
